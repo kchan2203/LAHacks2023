89bf673bba12d258d0dd604dbd0d973b7315499e
Vtewari2311 <vtewari@uwm.edu>
2023-04-23 03:52:05 -0500

    modifications to main

--- objectdetection.py
+++ objectdetection.py
@@ -117,11 +117,6 @@ def detect_and_sort_food_objects(image_path, detect_fn, category_index, min_scor
 #from google.colab import drive
 #drive.mount('/content/drive')
 
-import cv2
-import numpy as np
-import tensorflow as tf
-from datetime import datetime
-
 def load_yolo_model():
     # Load the YOLOv3 model
     model = tf.keras.models.load_model('yolov3.h5')
@@ -268,32 +263,37 @@ def load_yolo_model():
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    # Assuming that you have a model object named `model`
-    tf.saved_model.save(model, 'saved_model')
+        tf.saved_model.save(model, 'saved_model')
 
-# Load the input image
-image_path = 'input.jpg'
-detect_fn = tf.saved_model.load('saved_model')
-category_index = {
-    1: {'id': 1, 'name': 'apple'},
-    2: {'id': 2, 'name': 'banana'},
-    3: {'id': 3, 'name': 'orange'},
-    4: {'id': 4, 'name': 'pear'},
-    5: {'id': 5, 'name': 'watermelon'},
-    6: {'id': 6, 'name': 'tomato'},
-    7: {'id': 7, 'name': 'potato'},
-    8: {'id': 8, 'name': 'carrot'},
-    9: {'id': 9, 'name': 'broccoli'},
-    10: {'id': 10, 'name': 'pizza'},
-    11: {'id': 11, 'name': 'cake'},
-    12: {'id': 12, 'name': 'donut'},
-    13: {'id': 13, 'name': 'chair'}
-}
-min_score_thresh = 0.5
+        return model, class_names, anchors
 
-# Detect and sort the food objects in the input image
-food_objects_sorted = detect_and_sort_food_objects(image_path, detect_fn, category_index, min_score_thresh)
+def main():
+    # Load the input image
+    image_path = 'input.jpg'
+    detect_fn = tf.saved_model.load('saved_model')
+    category_index = {
+        1: {'id': 1, 'name': 'apple'},
+        2: {'id': 2, 'name': 'banana'},
+        3: {'id': 3, 'name': 'orange'},
+        4: {'id': 4, 'name': 'pear'},
+        5: {'id': 5, 'name': 'watermelon'},
+        6: {'id': 6, 'name': 'tomato'},
+        7: {'id': 7, 'name': 'potato'},
+        8: {'id': 8, 'name': 'carrot'},
+        9: {'id': 9, 'name': 'broccoli'},
+        10: {'id': 10, 'name': 'pizza'},
+        11: {'id': 11, 'name': 'cake'},
+        12: {'id': 12, 'name': 'donut'},
+        13: {'id': 13, 'name': 'chair'}
+    }
+    min_score_thresh = 0.5
 
-# Print the sorted food objects to the console
-for obj in food_objects_sorted:
-    print(obj)
+    # Detect and sort the food objects in the input image
+    food_objects_sorted = detect_and_sort_food_objects(image_path, detect_fn, category_index, min_score_thresh)
+
+    # Print the sorted food objects to the console
+    for obj in food_objects_sorted:
+        print(obj)
+
+if __name__ == '__main__':
+    main()
